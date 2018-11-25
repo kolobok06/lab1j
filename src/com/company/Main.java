0067c2819863a2f099f2328e431fce87a24d9e7a
kolobok06 <kolob060598@gmail.com>
2018-11-25 18:26:54 +0300

    commmmmit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        GUI guiMain = new GUI();
-        guiMain.setVisible(true);
+        GUI gui = new GUI();
+        gui.setVisible(true);
     }
 }
